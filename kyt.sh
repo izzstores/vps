8dce0a8a983ffc6a4e64c1997597b9c6aa167dbe
izzstores <158043117+izzstores@users.noreply.github.com>
2024-02-03 21:30:51 +0700

    Update kyt.sh

--- kyt.sh
+++ kyt.sh
@@ -15,14 +15,14 @@ mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/izzstores/vps/main/limit/kyt.zip
+wget -q -O kyt.zip "https://raw.githubusercontent.com/izzstores/vps/main/limit/bot-panel.zip"
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
-#isi data
+clear
 echo ""
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          ADD BOT PANEL          \e[0m"
+echo -e " \e[1;97;101m             ADD BOT PANEL              \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
 echo -e "${grenbo}[*] Creat Bot and Token Bot : @BotFather${NC}"
